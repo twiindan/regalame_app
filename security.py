7a5906ae2c2c84ff93853e5ab91dea583f3f95e9
toni.robres <twiindan@gmail.com>
2025-12-20 00:25:10 +0100

    trunk password

--- security.py
+++ security.py
@@ -6,4 +6,4 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 def get_password_hash(password: str) -> str:
-    return pwd_context.hash(password)
+    return pwd_context.hash(password[:72])
